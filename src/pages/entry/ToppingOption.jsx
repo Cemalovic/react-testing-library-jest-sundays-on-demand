0dac91030b29d74262b71eb40ab5b4363f0445ae
Milos Cemalovic <Milos.Cemalovic@rs.ey.com>
2022-04-28 17:13:28 +0200

    Test and code ToppingsOption with Context for toppings subTotal and implementing React Bootstrap Checkbox - Lesson 72

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,13 +1,21 @@
+import Form from 'react-bootstrap/Form'
 import Col from 'react-bootstrap/Col'
 
-const ToppingOption = ({ name, imagePath }) => {
+const ToppingOption = ({ name, imagePath, updateItemCount }) => {
+  const onCheckbox = (e) => {
+    updateItemCount(name, e.target.checked ? 1 : 0)
+  }
+
   return (
-    <Col xs={12} sm={6} md={4} lg={3}>
+    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
-        style={{ textAlign: 'center' }}
+        style={{ width: '75%' }}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type='checkbox' label={name} onChange={onCheckbox} />
+      </Form.Group>
     </Col>
   )
 }
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event'
 import { render, screen } from '../../../test-utils/testing-library-utils'
 import Options from '../Options'
 
-test('should update scoop sbutotal when scoops change', async () => {
+test('should update scoop subtotal when scoops change', async () => {
   render(<Options optionType='scoops' />)
 
   // make sure total starts out $0.00
@@ -25,4 +25,39 @@ test('should update scoop sbutotal when scoops change', async () => {
   userEvent.clear(chocolateInput)
   userEvent.type(chocolateInput, '2')
   expect(scoopsSubtotal).toHaveTextContent('6.00')
+})
+
+test('should update topping subtotal when toppings change', async () => {
+  render(<Options optionType='toppings' />)
+
+  // make sure total starts out $0.00
+  const toppingsSubtotal = screen.getByText('Toppings total: $', {
+    exact: false
+  })
+  expect(toppingsSubtotal).toHaveTextContent('0.00')
+
+  // add cherries and check subtotal
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
+    name: 'Cherries'
+  })
+  expect(cherriesCheckbox).not.toBeChecked()
+
+  userEvent.click(cherriesCheckbox)
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
+
+  // add M&Ms and check subtotal
+  const mAndMsCheckbox = await screen.findByRole('checkbox', {
+    name: 'M&Ms'
+  })
+  expect(mAndMsCheckbox).not.toBeChecked()
+
+  userEvent.click(mAndMsCheckbox)
+  expect(toppingsSubtotal).toHaveTextContent('3.00')
+
+  expect(cherriesCheckbox).toBeChecked()
+
+  userEvent.click(cherriesCheckbox)
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
+
+  expect(cherriesCheckbox).not.toBeChecked()
 })
